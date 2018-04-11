a3274c58b32cedd3a97fe162273463695cca5b17
srinivasmeenavalli@gmail.com <srinivasmeenavalli@gmail.com>
2018-04-11 09:29:36 +0530

    hello

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function(bp) {
-  bp.hear('MENU_SEND_EX_03', (event, next) => {
+  bp.hear({}, (event, next) => {
     event.reply('#textWithQuickRepliesIcon', {
       icon1: '${process.env.BOT_BASE_URL}/button_red.png',
       icon2: '${process.env.BOT_BASE_URL}/button_blue.png',
